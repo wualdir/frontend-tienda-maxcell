53d8cb1db43d27edb85105ef80a6990a8cb65668
wualdir <wualdir22@gmail.com>
2026-04-25 14:57:44 -0500

    CORS abierto total

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api-maxcell.onrender.com'
+  apiUrl: 'https://api-maxcell.onrender.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://api-maxcell.onrender.com'
+  apiUrl: 'https://api-maxcell.onrender.com/api'
 };
